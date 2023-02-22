63a75561041693dcc581afd6fb5df8b9eb16dc57
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-22 16:00:31 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('ola mundo');
+});
+app.listen(3000, () => {
+  console.log();
+});
