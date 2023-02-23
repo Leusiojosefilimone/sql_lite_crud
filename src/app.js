34fa23ae705ae7c2e76d4e3a2998201633e68257
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-02-23 16:00:19 -0300

    imports controllers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
-/* eslint-disable linebreak-style */
 const express = require('express');
+const { createTable, insertPessoa } = require('./controllers/Pessoa');
 
 const app = express();
+app.use(express.json);
+
 app.get('/', (req, res) => {
   res.send('ola mundo');
 });
-app.listen(3000, () => {
-  console.log('Sevidor rodando na porta 3000');
+
+createTable();
+
+app.post('/pessoa', (req, res) => {
+  insertPessoa(req.body);
+  res.json({
+    StatusCode: 200,
+  });
+
+  app.listen(3000, () => {
+    console.log('Sevidor rodando na porta 3000');
+  });
 });
