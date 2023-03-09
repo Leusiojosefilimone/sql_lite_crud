4b93199daf8a82422264b68498f43c61a0a4075a
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-03-09 14:12:39 -0300

    imports  upate from imployees controllers

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 const routes = require('express').Router()
-const {getOne,getAll, post} = require('./controller/employeesController');
+const {getOne,getAll, post, update, Delete} = require('./controller/employeesController');
 //employees routes
 routes.get("/employees", getAll)
 routes.get("/employees/:id", getOne)
 routes.post("/employees/", post)
-routes.patch("/employees/");
-routes.delete("/employees/:id")
+routes.patch("/employees/", update);
+routes.delete("/employees/:id",Delete)
 
 module.exports = routes
